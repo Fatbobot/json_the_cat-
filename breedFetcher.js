d36823a1e084108d3b167bd6f7df06f28bac4c88
Paul Jean <Fatbobot@gmail.com>
2023-11-21 17:07:40 -0400

    lint breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,7 +5,7 @@ let breedInput = args[0];
 request(
   `https://api.thecatapi.com/v1/breeds/search?q=${breedInput}`,
   (error, response, body) => {
-    console.log("error:", error); // Print the error if one occurred
+    console.error(error); // Print the error if one occurred
     console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
     const data = JSON.parse(body); //parse page data into JSON
     if (data.length === 0) {

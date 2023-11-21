75e6d1ffac36ce93a72ccbb1721deb418428f39d
Paul Jean <Fatbobot@gmail.com>
2023-11-21 18:21:38 -0400

    modularize code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
-const args = process.argv.slice(2);
 const request = require("request");
-let breedInput = args[0];
-
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breedInput}`,
-  (error, response, body) => {
-    console.error(error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    const data = JSON.parse(body); //parse page data into JSON
-    if (data.length === 0) {
-      return console.log(`Error : Breed "${breedInput}" not found.`);
-    } else {
-      console.log(data[0].description);
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      const data = JSON.parse(body); //parse page data into JSON
+      if (data.length === 0) {
+        return console.log(`Error : Breed "${breedName}" not found.`);
+      } else {
+        const desc = data[0].description;
+        callback(null, desc);
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
